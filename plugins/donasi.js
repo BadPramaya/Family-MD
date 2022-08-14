a6415c4df3720dbb53787681bd83e82b01e58d00
x_pramaya_x <78118894+BadPramaya@users.noreply.github.com>
2022-08-14 11:42:30 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,11 +2,11 @@ const qrku = "https://telegra.ph/file/f4ee3c55fd67cbd3ea861.jpg"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [62895327934887]
-│ • Dana  [62895327934887]
+│ • Telkomsel [94770611075]
+│ • Dana  [94770611075]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895327934887
+│ > Ingin donasi? Wa.me/94770611075
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
